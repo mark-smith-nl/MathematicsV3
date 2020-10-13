a606ff610bb624a34bdb603380b5738e8148ee6b
idi <m.smithhva.nl@MacBook-Pro-van-msmith.local>
2020-10-13 16:02:28 +0200

    Added layout frontend

--- src/main/java/nl/smith/mathematics/configuration/MvcConfig.java
+++ src/main/java/nl/smith/mathematics/configuration/MvcConfig.java
@@ -10,7 +10,7 @@ public class MvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-         registry.addRedirectViewController("/ping", "/echo?value=I am active");
+         //registry.addRedirectViewController("/ping", "/echo?value=I am active");
     }
 
     @Override
